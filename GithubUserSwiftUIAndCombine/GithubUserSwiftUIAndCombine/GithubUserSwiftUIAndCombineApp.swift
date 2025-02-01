f8aae4904a4dafd944343793d1e45cdeab0215a0
vuthanhdo <thanhdo.vtb@gmail.com>
2025-02-01 18:06:44 +0700

    Init project

---
+++ GithubUserSwiftUIAndCombine/GithubUserSwiftUIAndCombine/GithubUserSwiftUIAndCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubUserSwiftUIAndCombineApp.swift
+//  GithubUserSwiftUIAndCombine
+//
+//  Created by Đô Vũ Thành on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct GithubUserSwiftUIAndCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
